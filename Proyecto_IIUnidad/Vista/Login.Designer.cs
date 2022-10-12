3204063714811c4180a557f73f90dae3cb079109
KatherineCalix <113080728+KatherineCalix@users.noreply.github.com>
2022-10-12 17:16:38 -0600

    Comentario
    Agrege espacio y un comentario

--- Proyecto_IIUnidad/Vista/Login.Designer.cs
+++ Proyecto_IIUnidad/Vista/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Vista
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Login";
+            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
