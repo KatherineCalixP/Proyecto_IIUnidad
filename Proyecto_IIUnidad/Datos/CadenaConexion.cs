dc0ff18ffc4941dd05265dfbdcf6c1e1936ed79c
KatherineCalix <113080728+KatherineCalix@users.noreply.github.com>
2022-10-17 02:46:50 -0600

    Clase del 13
    trabajamos en clases

---
+++ Proyecto_IIUnidad/Datos/CadenaConexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class CadenaConexion
+    {
+        //Instanciar objeto 
+        //clase estatica es para que esa clase no sea nwcesario instanciar un objeto se accede directamente 
+
+        //Propiedad para conectarnos a la base de datos
+        public static string Cadena = "Data Source=Localhost; Initial Catalo=MiFactura; User ID=root; Password=programacion3";
+    }
+}
---
+++ Proyecto_IIUnidad/Datos/UsuarioDatos.cs
@@ -0,0 +1,172 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Datos
+{
+    public class UsuarioDatos
+    {
+        public async Task<bool> LoginAsync(string codigo,string clave)
+        {
+            bool valido=false;
+
+            try
+            {
+                //va a permitir consultar el usuario y la clave
+                string sql = "SELECT 1 FROM Usuario WHERE Codigo=@Codigo AND Clave=@Clave;";
+               //conexion y using nos ayuda a cerrarla
+                using(MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using(MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        comando.Parameters.Add("@Codigo",MySqlDbType.VarChar,20).Value = codigo;
+                        comando.Parameters.Add("@Clave", MySqlDbType.VarChar, 120).Value = clave;
+
+                        //ejecutar
+                       valido =Convert.ToBoolean(  await comando.ExecuteScalarAsync());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return valido;
+        }
+
+        public async Task<DataTable> DevolverListaAsync()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELEC * FROM Usuario";
+
+                using (MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using (MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+
+                        //ejecutar           (CASTEAR       )Es como el convert
+                        MySqlDataReader dr =(MySqlDataReader) await comando.ExecuteReaderAsync();
+                        dt.Load(dr);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return dt;
+        }
+
+        //metodo para asinar nuevo usuario
+        public async Task<bool> InsertarAsync(Usuario usuario)
+        {
+            bool inserto = false;
+            try
+            {
+                //va a permitir consultar el usuario y la clave
+                string sql = "INSERT INTO usuario VALUES (@Codigo, @Nombre, @Clave, @Correo, @Rol, @EstActivo);";
+                //conexion y using nos ayuda a cerrarla
+                using (MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using (MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        comando.Parameters.Add("@Codigo", MySqlDbType.VarChar, 20).Value = usuario.Codigo;
+                        comando.Parameters.Add("@Nombre", MySqlDbType.VarChar, 50).Value = usuario.Nombre;
+                        comando.Parameters.Add("@Clave", MySqlDbType.VarChar, 120).Value = usuario.Clave;
+                        comando.Parameters.Add("@Correo", MySqlDbType.VarChar, 45).Value = usuario.Correo;
+                        comando.Parameters.Add("@Rol", MySqlDbType.VarChar, 20).Value = usuario.Rol;
+                        comando.Parameters.Add("@EstActivo", MySqlDbType.TinyText).Value = usuario.EstActivo;
+
+                        //ejecutar
+                        await comando.ExecuteNonQueryAsync();
+                        inserto=true;   
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return inserto;
+
+            //
+        }
+
+        //Actualizar
+        public async Task<bool> ActualizarAsync(Usuario usuario)
+        {
+            bool actulizo= false;
+            try
+            {
+                //va a permitir consultar el usuario y la clave
+                string sql = "UPDATE Usuario SET Nombre=@Nombre, Clave=@Clave, Correo=@Correo, Rol=@Rol, EstaAtivo=@EstActivo WHERE Codigo=@Codigo";
+                //conexion y using nos ayuda a cerrarla
+                using (MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using (MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        comando.Parameters.Add("@Codigo", MySqlDbType.VarChar, 20).Value = usuario.Codigo;
+                        comando.Parameters.Add("@Nombre", MySqlDbType.VarChar, 50).Value = usuario.Nombre;
+                        comando.Parameters.Add("@Clave", MySqlDbType.VarChar, 120).Value = usuario.Clave;
+                        comando.Parameters.Add("@Correo", MySqlDbType.VarChar, 45).Value = usuario.Correo;
+                        comando.Parameters.Add("@Rol", MySqlDbType.VarChar, 20).Value = usuario.Rol;
+                        comando.Parameters.Add("@EstActivo", MySqlDbType.TinyText).Value = usuario.EstActivo;
+
+                        //ejecutar
+                        await comando.ExecuteNonQueryAsync();
+                        actulizo = true;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+            return actulizo;    
+            
+        }
+
+        //borrar usuario
+        public async Task<bool> EliminarAsync(string codigo)
+        {
+            bool elimino = false;
+            try
+            {
+                string sql = "DELETE FROM Usuario WHERE Codigo = @Codigo;";
+
+                using (MySqlConnection _conexion = new MySqlConnection(CadenaConexion.Cadena))
+                {
+                    await _conexion.OpenAsync();
+                    using (MySqlCommand comando = new MySqlCommand(sql, _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        comando.Parameters.Add("@Codigo", MySqlDbType.VarChar, 20).Value = codigo;
+                        await comando.ExecuteNonQueryAsync();
+                        elimino = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return elimino;
+        }
+    }
+}
